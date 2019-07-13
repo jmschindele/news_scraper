3460f09a7e8e197e6be888244dbf62b63e2bbbf4
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-07-12 20:02:03 -0400

    update styling

--- public/app.js
+++ public/app.js
@@ -2,15 +2,15 @@
 
 $.getJSON("/articles", function(data){
     for (var i = 0; i < data.length; i++) {
-        $("#articles").append("<p data-id='" + data[i]._id + " '>" + data[i].Headline + " </p> <a href="+ data[i].URL + " target='_blank'><button> View Article </button></a>");
+        $("#articles").prepend("<div class='article-card' data-id='"+ data[i]._id +"'> <p data-id='" + data[i]._id + " '>" + data[i].Headline + " </p> <a href="+ data[i].URL + " target='_blank'><button class='btn btn-sm btn-primary my-1'> View Article </button></a> </div>");
     }
 });
 
 
 //Whenever someone clicks a p tag
-$(document).on("click", "p", function(){
+$(document).on("click", "div.article-card", function(){
     $('#comments').empty();
-    // Save the id from the p tag
+    // Save the id from the div tag
     var thisId = $(this).attr("data-id");
     
 
@@ -25,11 +25,11 @@ $(document).on("click", "p", function(){
         //The title fo the article
         $("#comments").append("<h2>" + data.Headline + "</h2>");
         //An input to enter a new title
-        $("#comments").append("<input id='titleinput' name='title'>");
+        $("#comments").append("<input class='form-control my-1' place='Title' id='titleinput' name='title'>");
         // A textarea to add a new note body
-        $('#comments').append("<input id='bodyinput' name='title'>");
+        $('#comments').append("<textarea class='form-control my-1' place-holder='Comment' rows='4' id='bodyinput' name='title'>");
         // A button to submit a new note, wiuth the id of the article saved to it
-        $('#comments').append("<button data-id=' " + data._id + " ' id='savecomment'> Save Comment</ button>");
+        $('#comments').append("<button class='btn btn-success float-right my-2' data-id=' " + data._id + " ' id='savecomment'> Save Comment</ button>");
 
         if (data.comment) {
             $('#titleinput').val(data.comment.title);
@@ -40,15 +40,16 @@ $(document).on("click", "p", function(){
 
 
 //When you click the save note button
-$(document).on('click', '#savecomment', function() {
+$(document).on('click', '#savecomment', function(e) {
+    e.preventDefault();
     var thisId = $(this).attr('data-id');
 
     $.ajax({
         method: "POST",
-        url: "/articles" + thisId,
+        url: "/articles/" + thisId,
         data: {
-            title: $("#titleinput").val(),
-            body: $('#bodyinput').val()
+            title: $("#titleinput").val().trim(),
+            body: $('#bodyinput').val().trim()
         }
     })
 
